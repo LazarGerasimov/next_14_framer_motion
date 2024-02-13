8ec8427a200fb81841de06297201526a49cc0dd6
Laz <lazgerasimov@gmail.com>
2024-02-13 23:07:01 +0200

    setup infinite scroll

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,18 +1,38 @@
 "use client";
 
+import { fetchAnime } from "@/app/action";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
 
-// react intersection observer to keep track of user's viewport 
+let page = 2;
 
 function LoadMore() {
 
+  // react intersection observer to keep track of user's viewport  
   const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page)
+        .then((res) => {
+          setData([...data, ...res])
+          page++;
+        })
+    }
+  }, [inView, data]);
 
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
