5df2d6a027bf020bba635998b297c0be1781533c
Laz <lazgerasimov@gmail.com>
2024-02-13 23:21:00 +0200

    amend api call to return jsx element to apply the animation stagger on every fetched data batch

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -8,11 +8,13 @@ import AnimeCard, { AnimeProp } from "./AnimeCard";
 
 let page = 2;
 
+export type AnimeCard = JSX.Element;
+
 function LoadMore() {
 
   // react intersection observer to keep track of user's viewport  
   const { ref, inView } = useInView();
-  const [data, setData] = useState<AnimeProp[]>([]);
+  const [data, setData] = useState<AnimeCard[]>([]);
 
   useEffect(() => {
     if (inView) {
@@ -27,9 +29,7 @@ function LoadMore() {
   return (
     <>
       <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
-        {data.map((item: AnimeProp, index: number) => (
-          <AnimeCard key={item.id} anime={item} index={index} />
-        ))}
+        {data}
       </section>
       <section className="flex justify-center items-center w-full">
         <div ref={ref}>
